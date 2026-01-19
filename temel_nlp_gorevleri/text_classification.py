da9c1a4f0eba02d390c434e5d2dfe541e325b7ce
Furkan <furkanbulbul2222@gmail.com>
2026-01-20 01:22:38 +0300

    Train and evaluate Decision Tree spam classifier

--- temel_nlp_gorevleri/text_classification.py
+++ temel_nlp_gorevleri/text_classification.py
@@ -9,6 +9,12 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+
 # load dataset
 data = pd.read_csv("../datasets/spam.csv", encoding="Latin-1")
 
@@ -29,7 +35,6 @@ lemmatizer = WordNetLemmatizer()
 
 corpus = []
 
-text = "Fırkan 2134rq fas241 54wefgw"
 for i in range(len(text)):
 
     r = re.sub("[a-zA-Z]", " ",text[i]) # discard all characters without letter
@@ -40,7 +45,7 @@ for i in range(len(text)):
     
     r = [word for word in r if word not in stopwords.words("english")] # discard stopwords
     
-    r = [lemmatizer.temmatize(word) for word in r]
+    r = [lemmatizer.lemmatize(word) for word in r]
     
     r = "".join(r)
 
@@ -50,3 +55,27 @@ data["text2"] = corpus
 
 
 # model tranining and evalutaion
+
+X = data["text2"]
+y = data["label"] # target variable
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# feature extraction: bag of words
+cv = CountVectorizer()
+X_train_cv = cv.fit_transform(X_train)
+
+# classifier training: model training and evaluation
+dt = DecisionTreeClassifier()
+dt.fit(X_train_cv, y_train) # training
+
+x_test_cv = cv.transform(X_test)
+
+# prediction
+prediction = dt.predict(x_test_cv)
+
+c_matrix = confusion_matrix(y_test, prediction)
+print(f"Confusion matrix:\n{c_matrix}")
+
+accuracy = 100*(sum(sum(c_matrix)) - c_matrix[1,0] - c_matrix[0,1]) / sum(sum(c_matrix))
+print(accuracy)
