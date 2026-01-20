1048e7462f2456571c143b370d4bafb94ba9d0ad
Furkan <furkanbulbul2222@gmail.com>
2026-01-20 23:19:28 +0300

    Add sentiment classification evaluation metrics (confusion matrix, classification report)

--- temel_nlp_gorevleri/sentiment_analysis.py
+++ temel_nlp_gorevleri/sentiment_analysis.py
@@ -1,3 +1,4 @@
+# %%
 """
 Problem definition and dataset:
     https://github.com/pycaret/pycaret/blob/master/datasets/amazon.csv
@@ -5,6 +6,7 @@ Problem definition and dataset:
     classification comments in amazon dataset to positive or negative
 """
 
+# %%
 # import libraries
 import pandas as pd
 import nltk
@@ -14,15 +16,20 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
+from sklearn.metrics import confusion_matrix, classification_report
+
+# %%
 nltk.download("vader_lexicon")
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download("wordnet")
 nltk.download("omw-1.4")
 
+# %%
 # load dataset
 df = pd.read_csv("../datasets/amazon.csv")
 
+# %%
 # text cleaning, preprocessing
 lemmatizer = WordNetLemmatizer()
 def clean_preprocess_data(text):
@@ -40,4 +47,26 @@ def clean_preprocess_data(text):
 
     return process_text
 
+# %%
 df["reviewText2"] = df["reviewText"].apply(clean_preprocess_data)
+df[["reviewText", "reviewText2"]].head()
+
+# %%
+# sentiment analysis (nltk)
+analyzer = SentimentIntensityAnalyzer()
+
+def get_sentiments(text):
+    score = analyzer.polarity_scores(text)
+    sentiment = 1 if score["pos"] > 0 else 0
+
+    return sentiment
+
+df["sentiment"] = df["reviewText2"].apply(get_sentiments)
+df[["reviewText", "reviewText2"]].head()
+
+# %%
+cm = confusion_matrix(df["Positive"], df["sentiment"])
+print(f"Confusion matrix: {cm}")
+
+cr = classification_report(df["Positive"], df["sentiment"])
+print(f"Classification report: {cr}")
